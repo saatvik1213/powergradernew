c6c60e8469d804e5ba793e9cd29864a26246cbf2
Anadi Aakar Dewan <anadi.dewan@revmaxxtec.com>
2024-10-26 16:51:29 -0700

    added db connection and qwuery set up for login

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,14 +1,39 @@
+"use client"
 import { useRouter } from "next/navigation";
+import { client } from "../database";
 
+
+import react, {useState} from 'react'
 export default function LoginPage({ onClose }) {
   const router = useRouter();
-
   const handleLogin = (e) => {
     e.preventDefault();
+    try {
+      // Run a SELECT query on the users table
+      const result = client.query('SELECT * FROM users where user_id == $1',[id]);
+
+      user = result.rows[0]
+      var username = user.id;
+      if (user.role === "Grader"){
+        router.push(`/userPage/${username}`);
+      }
+      // Respond with the result rows
+      // if (result.rows.length === 0) {
+      //   return res.status(404).json({ error: 'User not found' });
+      // }
+    
+      const user = result.rows[0];
+    } 
+    catch (error) {
+      console.error('Database Error:', error);
+      // res.status(500).json({ error: 'Database retrieval error' });
+    }
+    
+
     // Perform login logic here (e.g., authentication)
-    var username = "johndoe"; // Assume the username is "johndoe"// needs to be pulled from database 
+     // Assume the username is "johndoe"// needs to be pulled from database 
     // After successful login, navigate to [userPage]
-    router.push(`/userPage/${username}`);
+ 
     onClose(); // Close the login modal if needed
   };
 
@@ -19,7 +44,6 @@ export default function LoginPage({ onClose }) {
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
         >
-          ✕
         </button>
 
         <h1 className="text-3xl font-bold mb-6">Login to PowerGrader</h1>
@@ -49,6 +73,7 @@ export default function LoginPage({ onClose }) {
             />
           </div>
 
+
           <button
             type="submit"
             className="w-full px-4 py-2 mt-4 font-semibold text-white bg-blue-600 rounded-md hover:bg-blue-700"
@@ -60,6 +85,7 @@ export default function LoginPage({ onClose }) {
         <p className="mt-4 text-gray-600">
           Don’t have an account? <a href="/signup" className="text-blue-600">Sign Up</a>
         </p>
+       
       </div>
     </div>
   );
