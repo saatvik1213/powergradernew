cc710f28bbb41746b216f697f387ae67dbe5c0ac
saatvik1213 <Saatvik1213@gmail.com>
2024-10-27 00:49:34 -0700

    changed smthing

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,6 +37,49 @@ async function getuserid(useremail) {
 			
 		});
 }
+async function createClass(classname, userid) {
+    const response = await fetch("http://localhost:5000/class", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: classname,
+            admin: userid
+        })
+    });
+
+    if (!response.ok) {
+        console.error("Error:", response.statusText);
+        return null;
+    }
+
+    const data = await response.json();
+    console.log("Response data:", data);
+    return data;
+}
+
+async function createuser(useremail, username, classid)
+{
+	fetch("http://localhost:5000/set_grader", {
+		method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: username,
+            email: useremail,
+			classid: classid
+        })
+	
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+			return data;
+			
+		});
+}
 
 
 
@@ -64,9 +107,11 @@ export default async function RootLayout({ children }) {
 	const user = session?.user;
 	const username = user?.name;
 	const email = user?.email;
-	
-	
-	const role =  await checkgrader(await getuserid(email));
+	classid.id = await createClass("test", "1");
+	createuser(email, username, classid);
+	userid = await getuserid(email);
+	const createClass = await createClass("test", userid);
+	const role =  await checkgrader();
 	return (
 		<html lang="en">
 			<body
